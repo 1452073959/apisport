469963f1814431f17f3ea07e97196859b5f77729
cc <1452073959@qq.com>
2020-06-05 17:45:03 +0800

    1

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -123,21 +123,18 @@ class WechatController extends Controller
             // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
             Cache::put('key',$message );
             $order = SMemberOrder::where('ordernum',$message['out_trade_no'])->first();
-            if (!$order || $order->paid_at) { // 如果订单不存在 或者 订单已经支付过了
-                return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+            if (!$order || $order['paid_at']) { // 如果订单不存在 或者 订单已经支付过了
+                return ; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
             }
             if ($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
                 // 用户是否支付成功
-                if (array_get($message, 'result_code') === 'SUCCESS') {
-                    $order->paid_at = $data=date('y-m-d h:i:s',time());; // 更新支付时间为当前时间
+                    $order->paid_at = $data=date('Y-m-d h:i:s',time());; // 更新支付时间为当前时间
                     $order->status = 1;
-                    // 用户支付失败
-                } elseif (array_get($message, 'result_code') === 'FAIL') {
-                    $order->status = 0;
-                }
             } else {
+                $order->status = 0;
                 return $fail('通信失败，请稍后再通知我');
             }
+            $order->save();
 
         });
         return $response;
