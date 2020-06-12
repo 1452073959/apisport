9c05150dfc1d7773de907c5f74f0a49904271f2d
cc <1452073959@qq.com>
2020-06-12 12:37:42 +0800

    1

--- app/Models/LeaseTenancy.php
+++ app/Models/LeaseTenancy.php
@@ -11,4 +11,14 @@ class LeaseTenancy extends Model
     protected $table = 'lease_tenancy';
     public $timestamps = false;
 
+    protected static function boot()
+    {
+        parent::boot();
+        // 监听模型创建事件，在写入数据库之前触发
+        static::creating(function ($model) {
+                $model->name ='整租';
+                $model->description ='整租为每小时价格';
+        });
+    }
+
 }
